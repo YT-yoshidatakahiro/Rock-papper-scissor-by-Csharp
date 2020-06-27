80004ecbf5790c754eea82d7b1b5536880ecf616
YT-yoshidatakahiro <63303848+YT-yoshidatakahiro@users.noreply.github.com>
2020-06-27 22:50:27 +0900

    Add files via upload

---
+++ Janken1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace janken
+{
+    partial class Janken1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbJudge = new System.Windows.Forms.TextBox();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.cbJan1 = new System.Windows.Forms.ComboBox();
+            this.cbJan2 = new System.Windows.Forms.ComboBox();
+            this.tbJudge2 = new System.Windows.Forms.TextBox();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbJudge
+            // 
+            this.tbJudge.Font = new System.Drawing.Font("メイリオ", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.tbJudge.Location = new System.Drawing.Point(40, 187);
+            this.tbJudge.Name = "tbJudge";
+            this.tbJudge.Size = new System.Drawing.Size(169, 43);
+            this.tbJudge.TabIndex = 2;
+            this.tbJudge.TabStop = false;
+            this.tbJudge.Text = "判定";
+            this.tbJudge.TextChanged += new System.EventHandler(this.tbJudge_TextChanged);
+            // 
+            // btnRun
+            // 
+            this.btnRun.Font = new System.Drawing.Font("メイリオ", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnRun.Location = new System.Drawing.Point(156, 122);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(169, 37);
+            this.btnRun.TabIndex = 3;
+            this.btnRun.TabStop = false;
+            this.btnRun.Text = "じゃんけん実行";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // cbJan1
+            // 
+            this.cbJan1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbJan1.Font = new System.Drawing.Font("メイリオ", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.cbJan1.FormattingEnabled = true;
+            this.cbJan1.Location = new System.Drawing.Point(40, 57);
+            this.cbJan1.Name = "cbJan1";
+            this.cbJan1.Size = new System.Drawing.Size(158, 39);
+            this.cbJan1.TabIndex = 4;
+            this.cbJan1.TabStop = false;
+            this.cbJan1.SelectedIndexChanged += new System.EventHandler(this.cbJan1_SelectedIndexChanged);
+            // 
+            // cbJan2
+            // 
+            this.cbJan2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbJan2.Font = new System.Drawing.Font("メイリオ", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.cbJan2.FormattingEnabled = true;
+            this.cbJan2.Location = new System.Drawing.Point(267, 57);
+            this.cbJan2.Name = "cbJan2";
+            this.cbJan2.Size = new System.Drawing.Size(158, 39);
+            this.cbJan2.TabIndex = 5;
+            this.cbJan2.TabStop = false;
+            this.cbJan2.SelectedIndexChanged += new System.EventHandler(this.cbJan2_SelectedIndexChanged);
+            // 
+            // tbJudge2
+            // 
+            this.tbJudge2.Font = new System.Drawing.Font("メイリオ", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.tbJudge2.Location = new System.Drawing.Point(256, 187);
+            this.tbJudge2.Name = "tbJudge2";
+            this.tbJudge2.Size = new System.Drawing.Size(169, 43);
+            this.tbJudge2.TabIndex = 6;
+            this.tbJudge2.TabStop = false;
+            this.tbJudge2.Text = "判定";
+            this.tbJudge2.TextChanged += new System.EventHandler(this.tbJudge2_TextChanged);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Font = new System.Drawing.Font("メイリオ", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnStop.Location = new System.Drawing.Point(156, 252);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(169, 37);
+            this.btnStop.TabIndex = 7;
+            this.btnStop.TabStop = false;
+            this.btnStop.Text = "やめる";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // Janken1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.tbJudge2);
+            this.Controls.Add(this.cbJan2);
+            this.Controls.Add(this.cbJan1);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.tbJudge);
+            this.Name = "Janken1";
+            this.Text = "じゃんけん";
+            this.Load += new System.EventHandler(this.Janken1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox tbJudge;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.ComboBox cbJan1;
+        private System.Windows.Forms.ComboBox cbJan2;
+        private System.Windows.Forms.TextBox tbJudge2;
+        private System.Windows.Forms.Button btnStop;
+    }
+}
+
---
+++ Janken1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace janken
+{
+    public partial class Janken1 : Form
+    {
+        public Janken1()
+        {
+            InitializeComponent();
+            ControlBox = false;
+        }
+        /// <summary>
+        /// 定義物
+        /// </summary>
+        public string[] janken1 = {"グー","チョキ","パー"};
+        public string[] kekka1 = { "勝ち", "負け"};
+        public string kekka2 ="あいこ";
+        /// <summary>
+        /// 実行ボタン
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            //あいこ
+            if (cbJan1.Text.Equals(cbJan2.Text))
+            {
+                tbJudge.Text = kekka2;
+                tbJudge2.Text = kekka2;
+            }
+            else
+            {
+                if (cbJan1.Text == janken1[0] && cbJan2.Text == janken1[1])　
+                {
+                    tbJudge.Text = kekka1[0];
+                    tbJudge2.Text = kekka1[1];
+                }
+                else if (cbJan1.Text == janken1[1] && cbJan2.Text == janken1[2])
+                {
+                    tbJudge.Text = kekka1[0];
+                    tbJudge2.Text = kekka1[1];
+                }
+                else if (cbJan1.Text == janken1[2] && cbJan2.Text == janken1[1])
+                {
+                    tbJudge.Text = kekka1[0];
+                    tbJudge2.Text = kekka1[1];
+                }
+                else
+                {
+                    tbJudge.Text = kekka1[1];
+                    tbJudge2.Text = kekka1[0];
+                }
+            }
+        }
+        /// <summary>
+        /// 開始時
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Janken1_Load(object sender, EventArgs e)
+        {
+            for (int i=0;i< janken1.Length;i++)
+            {
+                cbJan1.Items.Add(janken1[i]);
+
+                cbJan2.Items.Add(janken1[i]);
+            }
+        }
+        /// <summary>
+        /// リスト選択時
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void cbJan1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            tbJudge.Text = "";
+            tbJudge2.Text = "";
+        }
+
+        private void cbJan2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            tbJudge.Text = "";
+            tbJudge2.Text = "";
+        }
+        /// <summary>
+        /// 終了
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        /// <summary>
+        /// 不使用
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void tbJudge_TextChanged(object sender, EventArgs e)
+        {
+        }
+        private void tbJudge2_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+    }
+}
